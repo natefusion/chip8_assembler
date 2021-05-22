2a82bfbc68d19f86a9bb19c739e4b188e21c5552
natefusion <n8thefusion@gmail.com>
2021-05-21 21:45:31 -0400

    Lots of things, will not explain

--- src/main.rs
+++ src/main.rs
@@ -1,128 +1,67 @@
 #[allow(non_camel_case_types)]
 mod token;
-//mod lexer;
-//mod scanner;
 
-use std::{fs::File, io::{/*Write,*/ BufReader, BufRead}, env, process, result::Result, collections::HashMap};
-use crate::token::{Token, Token::*, TokenType, TokenType::*};
+use std::{fs, env, process};
+use crate::token::{Token, Category::*, Keyword::*};
+use lazy_static::lazy_static;
 
-use std::collections::hash_map::DefaultHasher;
-use std::hash::{Hash, Hasher};
 
-fn tokenize(lexeme: &str) -> Result<TokenType, String> {
-    let c = |x| lexeme.chars().nth(x);
 
-    match c(0).unwrap() {
-        '%' => match token::registers(lexeme) {
-            Some(x) => Ok(TokenType::Register(*x)),
-            None    => Err(format!("Expected a Register, found: {}", lexeme)),
-        },
+fn lexer(tokenstream: &'static str) -> Vec<Token> {
+    tokenstream
+        .lines()
+        .enumerate()
+        .flat_map(|(line, string)| {
+            // Comments go until the end of the line and are ignored by the parser
+            let delim = match string.find(';') {
+                Some(x) => x,
+                _ => string.len(),
+            };
 
-        '0'..='9' => {
-            let (index, radix) = if let Some('x') = c(1) { (2, 16) } else { (0, 10) };
-
-            match usize::from_str_radix(&lexeme[index..], radix) {
-                Ok(n) => Ok(TokenType::Number(n)),
-                Err(_) => Err(format!("Expected a number, found: {}", lexeme)),
-            }
-        },
-
-        _ => {
-            Ok(if let Some(x) = token::MNEMONICS.get(&lexeme) {
-                TokenType::Mnemonic(*x)
-            } else if let Some(x) = token::MACROS.get(&lexeme) {
-                TokenType::Macro(*x)
-            } else {
-                TokenType::Identifier({
-                    let mut s = DefaultHasher::new();
-                    lexeme.hash(&mut s);
-                    s.finish()
-                })
-            })
-        },
-    }
+            string[..delim]
+                .split_ascii_whitespace()
+                .enumerate()
+                .map(|(ch, raw)| Token::new(raw, line, ch))
+                .collect::<Vec<Token>>()
+                
+        }).collect()
 }
 
-fn scan(token: TokenType) {
-    match token {
-        Mnemonic(x) => { instructions.push((*x, vec![], vec![])); },
-        
-        Register(x) => {
-            if let V(n) = x { instructions.last_mut().unwrap().2.push(*n); }
-            instructions.last_mut().unwrap().1.push(*x);
-        },
-            
-        Number(x) => { instructions.last_mut().unwrap().2.push(*x); },
+/*
+fn parser(tokenlist: &[Token]) -> Vec<Instruction> {
+}
+*/
 
-        _ => {},
-    }
+fn load(path: Option<String>) -> String {
+    match path {
+        Some(x) => match fs::read_to_string(x) {
+            Ok(file) => file.trim().to_string(),
+            Err(_) => { eprintln!("Cannot read file"); process::exit(1); }},
+        None => { eprintln!("Please enter a file"); process::exit(1); }}
 }
 
 fn main() {
+    // Implement variable handling as part of parser as a variable in the parser function
     // remember that the max size is 4096 bytes!!
-    // No more stupid little 'a annotations. yay!!
-    let (_filename, file) = {
-        match env::args().nth(1) {
-            Some(filename) => {
-                match File::open(&filename) {
-                    Ok(file) => (filename, BufReader::new(file)),
-                    Err(_) => { eprintln!("Cannot read file"); process::exit(1); }}},
+    lazy_static! { static ref FILE: String = load(env::args().nth(1)); }
 
-            None => { eprintln!("Please enter a file"); process::exit(1); }}
-    };
+    let tokenlist = lexer(&FILE);
 
-    let mut tokens: Vec<TokenType> = vec![];
-
-    // Iterates through the file by line
-    // Splits each line by whitespace into chunks
-    // Tokenizes each chunk
-    for (index, line) in file.lines().enumerate().map(|(i,l)| (i, l.unwrap())) {
-        tokens.append(
-            &mut line[..(if let Some(x) = line.find(';') { x } else { line.len() })] // ';' represents the start of a comment
-            .split_ascii_whitespace()
-            .map(|l| l.to_ascii_lowercase())
-            .map(|lexeme| match tokenize(&lexeme) {
-                Ok(token) => token,
-                Err(msg)  => { eprintln!("{} on line {}", msg, index+1); process::exit(1); },
-            }).collect()
-        );
-    }
-
-    let mut iter = tokens.iter().peekable();
-    let mut variables = HashMap::new();
-    let mut instructions: Vec<(Token, Vec<Token>, Vec<usize>)> = vec![];
-    
-    while let Some(token) = iter.next() {
-        match token {
-            Identifier(x) => if let Some(t) = variables.get(x) { scan(token); },
-            
-            Macro(x) => {
-                if let Some(Identifier(i)) = iter.next() {
-                    match (x, iter.peek()) {
-                        (Alias | Const, Some(Register(_)) | Some(Number(_))) => { variables.insert(i, *iter.next().unwrap()); },
-                        (Colon, _) => { variables.insert(i, Number((instructions.len()) * 2 + 0x200)); },
-                        _ => {},
-                    }
-                }
-            },
-
-            _ => scan(token),
-
-        }
+    for token in tokenlist.iter() {
+        println!("{}",token.raw);
     }
 
     /*
-    for token in tokens.iter() {
-        println!("type: '{:?}'\nlexeme: '{}'\nline: '{}'\n",
-                 token.token, token.lexeme, token.index
-        );
-    }
-    */
+    let mut instructions = parse(&tokens);
 
-    for (mnemonic, registers, arguments) in instructions.iter() {
-        print!("{:?} ", mnemonic);
-        registers.iter().for_each(|r| print!("{:?} ", r));
-        arguments.iter().for_each(|a| print!("{} ", a));
-        println!();
+    for ins in instructions.iter() {
+        println!("Instruction {{");
+        println!("\tidentifier: {}", ins.identifier.lexeme);
+        println!("\targuments: {}", ins.arguments.iter().fold(String::new(), |state, x| {
+            format!("{} {} ",state, x.lexeme)
+                
+        }));
+        println!("}}");
     }
+     */
 }
--- src/token.rs
+++ src/token.rs
@@ -1,145 +1,79 @@
-use std::fmt;
-use Token::*;
+use {Keyword::*, Category::*};
 
-pub fn mnemonic(lexeme: &str) -> Result<(TokenType, Token), String> {
-    match lexeme {
-        "clear"    => Ok((Mnemonic, Clear)),
-        "end"      => Ok((Mnemonic, End)),
-        "jump"     => Ok((Mnemonic, Jump)),
-        "jump0"    => Ok((Mnemonic, Jump0)),
-        "begin"    => Ok((Mnemonic, Begin)),
-        "neq"      => Ok((Mnemonic, Neq)),
-        "eq"       => Ok((Mnemonic, Eq)),
-        "set"      => Ok((Mnemonic, Set)),
-        "add"      => Ok((Mnemonic, Add)),
-        "or"       => Ok((Mnemonic, Or)),
-        "and"      => Ok((Mnemonic, And)),
-        "xor"      => Ok((Mnemonic, Xor)),
-        "sub"      => Ok((Mnemonic, Sub)),
-        "shr"      => Ok((Mnemonic, Shr)),
-        "subr"     => Ok((Mnemonic, Subr)),
-        "shl"      => Ok((Mnemonic, Shl)),
-        "rand"     => Ok((Mnemonic, Rand)),
-        "draw"     => Ok((Mnemonic, Draw)),
-        "writebcd" => Ok((Mnemonic, Writebcd)),
-        "write"    => Ok((Mnemonic, Write)),
-        "read"     => Ok((Mnemonic, Read)),
-        x          => Err(format!("Expected a mnemonic, found: {}", x)),
-    }
+pub struct Token {
+    pub category: Category,
+    pub raw: &'static str,
+    pub line: usize,
+    pub ch: usize,
 }
 
-pub fn register(lexeme: &str) -> Result<(TokenType, Token), String> {
-    match lexeme {
-        "%v0"  => Ok((Register, V(0x0))),
-        "%v1"  => Ok((Register, V(0x1))),
-        "%v2"  => Ok((Register, V(0x2))),
-        "%v3"  => Ok((Register, V(0x3))),
-        "%v4"  => Ok((Register, V(0x4))),
-        "%v5"  => Ok((Register, V(0x5))),
-        "%v6"  => Ok((Register, V(0x6))),
-        "%v7"  => Ok((Register, V(0x7))),
-        "%v8"  => Ok((Register, V(0x8))),
-        "%v9"  => Ok((Register, V(0x9))),
-        "%va"  => Ok((Register, V(0xA))),
-        "%vb"  => Ok((Register, V(0xB))),
-        "%vc"  => Ok((Register, V(0xC))),
-        "%vd"  => Ok((Register, V(0xD))),
-        "%ve"  => Ok((Register, V(0xE))),
-        "%vf"  => Ok((Register, V(0xF))),
-        "%i"   => Ok((Register, I)),
-        "%dt"  => Ok((Register, DT)),
-        "%st"  => Ok((Register, ST)),
-        "%key" => Ok((Register, Key)),
-        x      => Err(format!("Expected a register, found: {}", x)),
-    }
-}
 
-pub fn macro(lexeme: &str) -> Result<(TokenType, Token), String> {
-    match lexeme {
-        "alias" => Ok((Macro, Alias)),
-        "const" => Ok((Macro, Const)),
-        ":"     => Ok((Macro, Colon)),
-        x       => Err(format!("Expeceted a macro, found: {}", x)),
-    }
-}
-
-pub struct TokenInfo {
-    pub tt: TokenType,
-    pub t: Token,
-    pub l: String,
-    pub i: usize,
-}
-
-#[derive(Copy, Clone)]
-pub enum Token {
-    // Registers
-    V(usize), I, DT, ST, Key,
-
-    // Mnemonics
-    Clear, End, Jump, Jump0, Begin, Neq,
+pub enum Keyword {
+    V, I, Dt, St, Key,
+    
+    Clear, Return, Jump, Jump0, Call, Neq,
     Eq, Set, Add, Or, And, Xor, Sub, Shr,
     Subr, Shl, Rand, Draw, Writebcd, Write,
     Read,
-
-    // Macros
-    Colon, Alias, Const,
+    
+    Colon, Define,
 }
 
-#[derive(Copy, Clone)]
-pub enum TokenType {
-    Identifier(u64),
-    Mnemonic(Token),
-    Register(Token),
-    Macro(Token),
-    Number(usize),
+pub enum Category {
+    Function(Keyword),
+    Definition(Keyword),
+    Register(Keyword),
+    Number,
+    Identifier,
 }
 
-impl fmt::Debug for Token {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_str(
-            match &self {
-                Token::Colon => "Colon",
-                Token::Alias => "Alias",
-                Token::Const => "Const",
-                Token::Clear => "Clear",
-                Token::End => "End",
-                Token::Jump => "Jump",
-                Token::Jump0 => "Jump0",
-                Token::Begin => "Begin",
-                Token::Neq => "Neq",
-                Token::Eq => "Eq",
-                Token::Set => "Set",
-                Token::Add => "Add",
-                Token::Or => "Or",
-                Token::And => "And",
-                Token::Xor => "Xor",
-                Token::Sub => "Sub",
-                Token::Shr => "Shr",
-                Token::Subr => "Subr",
-                Token::Shl => "Shl",
-                Token::Rand => "Rand",
-                Token::Draw => "Draw",
-                Token::Writebcd => "Writebcd",
-                Token::Write => "Write",
-                Token::Read => "Read",
-                Token::V(_) => "V",
-                Token::I => "I",
-                Token::DT => "DT",
-                Token::ST => "ST",
-                Token::Key => "Key",
-            }
-        )
+impl Token {
+    pub fn new(raw: &'static str, line: usize, ch: usize) -> Self {
+        Token { category: Self::tokenize(raw), raw, line, ch }
     }
-}
 
-impl fmt::Debug for TokenType {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match &self {
-            TokenType::Number(x) => f.write_fmt(format_args!("Number:{:?}",x)),
-            TokenType::Mnemonic(x) => f.write_fmt(format_args!("Mnemonic:{:?}",x)),
-            TokenType::Register(x) => f.write_fmt(format_args!("Register:{:?}",x)),
-            TokenType::Macro(x) => f.write_fmt(format_args!("{:?}",x)),
-            TokenType::Identifier(x) => f.write_fmt(format_args!("Identifier: {}",x)),
+    fn tokenize(raw: &str) -> Category {
+        match raw.chars().nth(0).unwrap() {
+            '0'..='9' => Number,
+            
+            'v' => Register(V),
+            'i' => Register(I),
+            ':' => Definition(Colon),
+            
+            'a'..='z' |
+            'A'..='Z' => match raw {
+                "clear"    => Function(Clear),
+                "return"   => Function(Return),
+                "jump"     => Function(Jump),
+                "jump0"    => Function(Jump0),
+                "call"     => Function(Call),
+                "neq"      => Function(Neq),
+                "eq"       => Function(Eq),
+                "set"      => Function(Set),
+                "add"      => Function(Add),
+                "or"       => Function(Or),
+                "and"      => Function(And),
+                "xor"      => Function(Xor),
+                "sub"      => Function(Sub),
+                "shr"      => Function(Shr),
+                "subr"     => Function(Subr),
+                "shl"      => Function(Shl),
+                "rand"     => Function(Rand),
+                "draw"     => Function(Draw),
+                "writebcd" => Function(Writebcd),
+                "write"    => Function(Write),
+                "read"     => Function(Read),
+                
+                "define" => Definition(Define),
+
+                "dt"  => Register(Dt),
+                "st"  => Register(St),
+                "key" => Register(Key),
+
+                _ => Identifier,
+            }
+
+            _ => Identifier,
         }
     }
 }
